6a80e055798cba18c3b97e642856ffda91408943
crazyyanchao <1141679423@qq.com>
2016-06-23 06:58:10 -0700

    deadlockexample

---
+++ src/chinahadoop/deadlock.java
@@ -0,0 +1,51 @@
+package chinahadoop;
+
+/**
+  * Created by root on 16-6-23.
+  */
+
+public class deadlock {
+
+    static Object obj1 = new Object(), obj2 = new Object();
+
+    static class ThreadA implements Runnable {
+        @Override
+        public void run() {
+            synchronized(obj1) {
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+                synchronized(obj2) {
+                    System.out.println("Should not be here");
+                }
+            }
+        }
+    }
+
+
+    static class ThreadB implements Runnable {
+        @Override
+        public void run() {
+            synchronized(obj2) {
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+                synchronized(obj1) {
+                    System.out.println("Should not be here");
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        new Thread(new ThreadA()).start();
+        new Thread(new ThreadB()).start();
+    }
+
+}
